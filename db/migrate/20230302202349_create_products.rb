48163ab60803eadbfccf252ad393ab8547b5e131
luccamicale <luccamicale2015@gmail.com>
2023-03-02 17:45:20 -0300

    We created a migrations

---
+++ db/migrate/20230302202349_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :image
+      t.decimal :price
+      t.text :description
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
