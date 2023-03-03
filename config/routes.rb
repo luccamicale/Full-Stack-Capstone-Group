58c418e5afb60acc6e0ea078abd6c5fa16428a02
ndorrh <ndorrh@gmail.com>
2023-03-03 17:31:46 +0100

    Add reservation to route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :products
+      resources :reservations
       post '/auth/login', to: 'authentication#login'
     end
   end
