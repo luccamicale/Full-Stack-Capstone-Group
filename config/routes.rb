dc8b787993af100c654a83e0d9e7fb40f047a9f8
luccamicale <luccamicale2015@gmail.com>
2023-03-06 16:12:32 -0300

    delete comments in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
   resources :reservations
   resources :users
   get 'root/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
   namespace :api do
     namespace :v1 do
       get 'reservations', to: 'reservations#index'
