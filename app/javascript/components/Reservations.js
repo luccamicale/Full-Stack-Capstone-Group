eafa84f587462037bd49d90dd4ffeb98c193f175
Oklukeok <lucasabermudez@gmail.com>
2023-03-06 16:39:50 -0600

    Created Navbar

---
+++ app/javascript/components/Reservations.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Reservations() {
+    return (
+      <div className="reservations">
+        <h1>Reservations</h1>
+      </div>
+    );
+  }
+  
+  export default Reservations;
