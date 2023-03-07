f3815794e0d343e7732e032ee28da1631572c8e7
ndorrh <ndorrh@gmail.com>
2023-03-07 22:53:17 +0100

    Update redux store to include reservation reducer

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import myReducer from './home/Home';
+import reservationSlice from './reservation/Reservation';
 
 const store = configureStore({
   reducer: {
     homes: myReducer,
+    reservations: reservationSlice,
   },
 });
 
