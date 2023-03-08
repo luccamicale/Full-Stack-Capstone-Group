369b2977e3338ff28cc5fcd1eacea33041a5f441
luccamicale <luccamicale2015@gmail.com>
2023-03-08 11:37:48 -0300

    create a form and input

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
 function Login() {
+
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-      <div className="login">
+      <section className="login">
         <h1>Login</h1>
-      </div>
+        <form className="Form"
+            onSubmit={handleSubmit}
+        >
+          
+            <input 
+                type='text' 
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+            <input
+                 type='password' 
+                 value={password}
+                 onChange={e => setPassword(e.target.value)}
+            />
+            <button>Login</button>
+        </form>
+      </section>
     );
   }
   
