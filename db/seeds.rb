8dbe1e3c0d910addb5b4a3448608a6fb73ca98b6
amare1990 <amaremek@gmail.com>
2023-03-08 22:14:55 +0300

    Add another seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@ product = Product.create!(name: "Model 1",
 image: "https://imageio.forbes.com/specials-images/imageserve/60c3a36876435a1bf9b4012e/Tesla-Model-S-Plaid/960x0.jpg?format=jpg&width=960",
 price: 0.1e5, description: "jgi  ghgh")
 
+product2 = Product.create!(name: "Model 2",
+image: "https://imageio.forbes.com/specials-images/imageserve/60c3a36876435a1bf9b4012e/Tesla-Model-S-Plaid/960x0.jpg?format=jpg&width=960",
+price: 20000, description: "Second tesla car")
+
