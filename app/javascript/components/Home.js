36ec3f1ef7c01770487bd952e56888dae41460e9
luccamicale <luccamicale2015@gmail.com>
2023-03-09 00:21:18 -0300

    create a carousel

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -3,48 +3,64 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchHomes } from '../redux/home/Home';
 import '../styles/home.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-import {useNavigate } from 'react-router-dom';
-// import { NavLink } from 'react-router-dom';
-// import Detail from './components/Detail';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
-  const home = useSelector((state) => state.homes);
+  const homes = useSelector((state) => state.homes);
   const dispatch = useDispatch();
-  console.log(home)
+  console.log(homes);
 
   const navigate = useNavigate();
-
 
   const handleDetailsClick = () => {
     const pathToDetail = '/Details';
     navigate(pathToDetail);
-   // <Link to="/Details"></Link>
   };
 
   useEffect(() => {
     dispatch(fetchHomes());
   }, [dispatch]);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '60px',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+        },
+      },
+    ],
+  };
+
   return (
     <div>
-      <h1 className='latest-model'>Latest Models</h1>
+      <h1 className="latest-model">Latest Models</h1>
       <hr></hr>
-      <h4 className='tesla-model'>Please select a Tesla Model</h4>
-      {home.map((home) =>
-(  <Link to={`${home.id}`} key={home.id}>
-        <div className='container'>
-            <img className='image' src={home.image}></img>
-            <h2>{home.name}</h2>
-            <h3>{home.description}</h3>
-        </div>
-
-        {/* </button> */}
-        </Link>)
-      )}
-
+      <h4 className="tesla-model">Please select a Tesla Model</h4>
+      <Slider {...settings}>
+        {homes.map((home) => (
+          <Link to={`${home.id}`} key={home.id}>
+            <div className="container">
+              <img className="image" src={home.image}></img>
+              <h2>{home.name}</h2>
+              <h3>{home.description}</h3>
+            </div>
+          </Link>
+        ))}
+      </Slider>
     </div>
-
   );
   }
 
