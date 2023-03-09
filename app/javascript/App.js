a53a22f906aa8f21faaf50a0764801af4ac04d73
ndorrh <ndorrh@gmail.com>
2023-03-09 14:31:12 +0100

    Add styling to reservation page

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar />
+      <NavBar setProduct={setProduct} />
 
       <Routes>
         <Route path="/" element={<Landing />} />
--- app/javascript/pages/NavBar.js
+++ app/javascript/pages/NavBar.js
@@ -4,7 +4,7 @@ import React from 'react';
 //import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-function NavBar() {
+function NavBar({setProduct}) {
  // const dispatch = useDispatch();
  // dispatch(retrieveRocket());
 
@@ -24,7 +24,7 @@ function NavBar() {
         </li>
 
         {<li>
-          <NavLink className="item" to="/reservationForm" >
+          <NavLink className="item" to="/reservationForm" onClick={() => setProduct(null)}>
             Reserve
           </NavLink>
         </li>}
