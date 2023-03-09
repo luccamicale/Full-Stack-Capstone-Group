56d22ea486b3f6f19faaa8ddb4a23f76d60bb6d9
ndorrh <ndorrh@gmail.com>
2023-03-09 16:21:11 +0100

    Add styling to reservation page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'reservations', to: 'reservations#index'
       post 'reservations', to: 'reservations#create'
+      delete 'reservations/:id', to: 'reservations#destroy'
       post 'signup', to: 'users#create'
       get 'users', to: 'users#index'
       get 'products', to: 'products#index'
