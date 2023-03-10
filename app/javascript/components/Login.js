4889e4964c8108de0368a5baa7519a194019561b
Oklukeok <lucasabermudez@gmail.com>
2023-03-10 14:54:54 -0600

    Login basic structure

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -1,12 +1,24 @@
 import React from "react";
-
+import logo from './img/logo.png';
 
 
 function Login() {
 
     return (
       <div className="login">
-        <h1>Login</h1>
+        <div className="logo"><img src={logo} alt="TESLA"/></div>
+        <div className="login-box">
+            <h1>Log in</h1>
+            <div className="loginform">
+                <form>
+                <div><input type="text" value="User name"/></div>
+                <div><input type="text" value="Password"/></div>
+                <button type="submit">Log in</button>
+                </form>
+                <p>Don't have an account?</p>
+                <p>Create Account</p>
+            </div>
+        </div>
       </div>
     );
   }
