d1c86c2fd3ca28fbdd1958896c0bee949959fc7c
Oklukeok <lucasabermudez@gmail.com>
2023-03-10 15:38:23 -0600

    basic login mobile style

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -1,19 +1,19 @@
 import React from "react";
 import logo from './img/logo.png';
-
+import './Login.css'
 
 function Login() {
 
     return (
       <div className="login">
-        <div className="logo"><img src={logo} alt="TESLA"/></div>
+        <div className="landinglogo"><img src={logo} alt="TESLA"/></div>
         <div className="login-box">
             <h1>Log in</h1>
             <div className="loginform">
-                <form>
+                <form className="loginformtag">
                 <div><input type="text" value="User name"/></div>
                 <div><input type="text" value="Password"/></div>
-                <button type="submit">Log in</button>
+                <p>Log in</p>
                 </form>
                 <p>Don't have an account?</p>
                 <p>Create Account</p>
