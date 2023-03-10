9aa5584209a0ae7c380ecd12b14f97d0439af0f6
Oklukeok <lucasabermudez@gmail.com>
2023-03-10 13:55:31 -0600

    Created routes for login page and add it to navbar

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -7,7 +7,7 @@ import Landing from './components/Landing';
 import Detail from './components/Detail';
 import Reservations from './components/Reservations';
 import ReservationForm from './components/ReservationForm';
-
+import Login from './components/Login';
 import ProductForm from './components/ProductForm';
 import { fetchHomes } from './redux/home/Home';
 
@@ -33,6 +33,8 @@ function App() {
         <Route path="/reservationForm" element={<ReservationForm product={product} user={user} />} />
         <Route path="/new-product" element={<ProductForm  /> } />
         <Route path="/reservationForm" element={<ReservationForm product={product} user={user} setProduct={setProduct} />} />
+      
+        <Route path="/login" element={<Login />} />
       </Routes>
 
     </div>
---
+++ app/javascript/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Login() {
+
+    return (
+      <div className="login">
+        <h1>Login</h1>
+      </div>
+    );
+  }
+
+  export default Login;
