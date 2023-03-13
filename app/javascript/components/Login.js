96942ec0acf00cbf051fd35b7100008c7ea169f1
Oklukeok <lucasabermudez@gmail.com>
2023-03-13 13:45:27 -0600

    Login success path

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -2,16 +2,26 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { fetchUsers } from '../redux/registration/Registration';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
 
+  const path = '/Home';
+  const navigate = useNavigate();
   const [username, setUsername] = useState('Username');
   const [password, setPassword] = useState('Password');
   const users = useSelector((state) => state.users.users);
-  const usernameArray = users.username.split()
-  if(users.username.includes(username)) {
-    e.preventDefault();
-    handleSubmit
+  console.log(`users.username= ${users}`)
+  //const usernameArray = users.username.split()
+  if(users.username === username) {
+  
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+      const userData = {username,password};
+    navigate(path);
+    }
+
+
     }
  
  
