7c2a4053b4805902e804f83de87b19fbd8be7f63
ndorrh <ndorrh@gmail.com>
2023-03-14 17:06:01 +0100

    Disable cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_controller.default_protect_from_forgery = false
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
