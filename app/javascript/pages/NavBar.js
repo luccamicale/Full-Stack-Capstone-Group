cecd71dbf36bc9b568bc3a1d1a0c25c80b75025b
ndorrh <ndorrh@gmail.com>
2023-03-14 14:21:02 +0100

    Add Logout functionality

--- app/javascript/pages/NavBar.js
+++ app/javascript/pages/NavBar.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import '../components/navbar.css'
 import logo from '../components/img/logo.gif';
 
 
-function NavBar({ setProduct }) {
+function NavBar({ setProduct, userId }) {
 
   const [clicked, setClicked] = useState(false)
+  const navigate = useNavigate()
 
   const handleClick = () => {
     setClicked(!clicked)
@@ -19,62 +20,67 @@ function NavBar({ setProduct }) {
 
   return (
     <div className='nav' id="navbar">
-       <div className="menuButton" id="menuButton" onClick={handleClick}><p>Menu</p></div>
-       <div className={`menu ${clicked ? 'active' : ''}`}>
+      <div className="menuButton" id="menuButton" onClick={handleClick}><p>Menu</p></div>
+      <div className={`menu ${clicked ? 'active' : ''}`}>
         <ul className="ul-nav">
 
-        <li className="link-item">
+          {(userId) && <> <li className="link-item">
             <NavLink className="item" to="/" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
-          <img src={logo} alt="TESLA" className='menulogo'/>
-          </NavLink>
-        </li>
-        <li className="link-item">
-            <NavLink className="item" to="/Home" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
-            Home
-          </NavLink>
-        </li>
+              <img src={logo} alt="TESLA" className='menulogo' />
+            </NavLink>
+          </li>
+            <li className="link-item">
+              <NavLink className="item" to="/Home" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
+                Home
+              </NavLink>
+            </li>
 
-        <li>
-            <NavLink className="link-item" to="/reservationForm" onClick={set} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
-            Reserve
-          </NavLink>
-        </li>
+            <li>
+              <NavLink className="link-item" to="/reservationForm" onClick={set} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
+                Reserve
+              </NavLink>
+            </li>
 
-        <li>
-            <NavLink className="link-item" to="/Reservations" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
-            Reservations
-          </NavLink>
-        </li>
+            <li>
+              <NavLink className="link-item" to="/Reservations" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
+                Reservations
+              </NavLink>
+            </li>
 
-        <li>
-          <NavLink className="link-item" to="/new-product" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
-            Add product
-          </NavLink>
-        </li>
+            <li>
+              <NavLink className="link-item" to="/new-product" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
+                Add product
+              </NavLink>
+            </li>
 
-        <li>
+            <li>
+              <NavLink className="link-item" to="/Products" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
+                Delete product
+              </NavLink>
+            </li>
 
-            <NavLink className="link-item" to="/login" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
-            Log in
-          </NavLink>
-        </li>
-        
-        <li>
-            <NavLink className="link-item" to="/signup" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
-            Sign Up
-          </NavLink>
-        </li>
-        
-        <li>
-          <NavLink className="link-item" to="/Products" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
-            Delete Product
-          </NavLink>
-        </li>
+            <li>
+              <NavLink className="link-item" to="/login" onClick={() => localStorage.clear('user')} style={{ color: '#fff', border: 'solid 1px red', backgroundColor: 'red', borderRadius: '5px' }}>
+                logout
+              </NavLink>
+            </li>
+          </>}
 
+          {(!userId) && <><li>
 
-     </ul>
+            <NavLink className="link-item" to="/" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
+              Log in
+            </NavLink>
+          </li>
 
-        </div>
+            <li>
+              <NavLink className="link-item" to="/signup" onClick={handleClick} style={({ isActive }) => (isActive ? { color: '#fff', border: 'solid 1px #1dbe28', backgroundColor: '#1dbe28' } : { color: 'black' })}>
+                Sign Up
+              </NavLink>
+            </li></>}
+        </ul>
+
+      </div>
     </div>
   );
 }
