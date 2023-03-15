7b2234f4ce61333d76fd4f76f0ed57e0fb28f759
ndorrh <ndorrh@gmail.com>
2023-03-15 07:05:36 +0100

    Update api base link

--- app/javascript/redux/reservation/Reservation.js
+++ app/javascript/redux/reservation/Reservation.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const url = 'https://localhost:3000/api/v1/reservations';
+const url = 'https://reserve-tesla-car.onrender.com/api/v1/reservations';
 
 export const fetchReservations = createAsyncThunk('FETCHRESERVATIONS', () => axios.get(url)
   .then((response) => {
